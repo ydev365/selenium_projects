4f44bd3afdff5307a2dd0b86fb02a779079c3ce6
ydev365 <163964407+ydev365@users.noreply.github.com>
2024-07-17 00:38:29 +0900

    Update selenium_code_that_works_on_my_laptop.py

--- selenium_code_that_works_on_my_laptop.py
+++ selenium_code_that_works_on_my_laptop.py
@@ -6,8 +6,8 @@ import time
 
 
 options = webdriver.ChromeOptions()
-options.add_argument('--start-maximized')
+options.add_argument('--start-maximized')# its for maximizing the screen of the website
 options.add_experimental_option("detach", True)
 
-driver=webdriver.Chrome(options=options)
+driver=webdriver.Chrome(options=options)#inside the options=options is the reason why the code works and makes the website stays open
 driver.get('https://www.naver.com/')
